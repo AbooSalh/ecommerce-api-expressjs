29402f5cf01c2fef881cfe0cb7ebff5b4dd0941f
AbooSalh <a.m0507979@gmail.com>
2025-03-06 15:43:07 +0200

    feat : category schema

---
+++ src/modules/category/category.module.ts
@@ -0,0 +1,23 @@
+import { Schema } from "mongoose";
+
+const categorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Category name is required"],
+      unique: [true, "Category name must be unique"],
+      minLength: [3, "Category name must be at least 3 characters"],
+      maxLength: [32, "Category name must be at most 32 characters"],
+    },
+    // api endpoint
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+  },
+  //   mongoose options
+  {
+    // timestamps for createdAt and updatedAt
+    timestamps: true,
+  }
+);
