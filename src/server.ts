1aa63a3ca9d8cf5909445c6a1d7f74a4e5449953
AbooSalh <a.m0507979@gmail.com>
2025-03-06 21:20:33 +0200

    feat: handling unmatched routes

--- src/server.ts
+++ src/server.ts
@@ -12,8 +12,15 @@ app.use(express.json()); // ✅ Middleware for parsing JSON
 
 dbConnection();
 
-app.use("/api", [categoryRouter]); // ✅ Mount router with a prefix ("/api")
-
+app.use("/api", [categoryRouter]);
+// handle all other unhandled routes
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`);
+  next(err);
+});
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ success: false, message: err.message });
+});
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
