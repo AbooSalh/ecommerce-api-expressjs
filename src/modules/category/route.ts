4702d94e0a62c2fa572358199b3ba75d48e0e06d
AbooSalh <a.m0507979@gmail.com>
2025-03-07 10:08:27 +0200

    feat: validation layer , wating for refactor

--- src/modules/category/route.ts
+++ src/modules/category/route.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express , { Request, Response, NextFunction } from "express";
 import { categoryController } from "./controller";
+import { param, validationResult } from "express-validator";
 const categoryRouter = express.Router();
 categoryRouter
   .route("/categories")
@@ -9,5 +10,19 @@ categoryRouter
   .route("/categories/:title")
   .get(categoryController.getOne)
   .put(categoryController.update)
-  .delete(categoryController.delete);
+  .delete(
+    // rules
+    param("title").exists().withMessage("Category title is required"),
+    // middleware catch
+    (req: Request, res: Response, next: NextFunction) => {
+      // finds the validation errors in this request and wraps them in an object
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+      } else {
+        next();
+      }
+    },
+    categoryController.delete
+  );
 export default categoryRouter;
