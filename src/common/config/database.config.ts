398b55e5e99ed88342f3c84607ee679ca415b07a
AbooSalh <a.m0507979@gmail.com>
2025-03-09 13:34:30 +0200

    refactor: update database connection handling to use object methods for better structure and error management

--- src/common/config/database.config.ts
+++ src/common/config/database.config.ts
@@ -1,10 +1,21 @@
 import mongoose from "mongoose";
 
-const dbConnection = () => {
-  mongoose
-    .connect(process.env.DB_URL as string, { dbName: process.env.DB_NAME })
-    .then((conn) => {
-      console.log(`Database Connected: ${conn.connection.host}`);
+const dbConnection = {
+  connect: () => {
+    mongoose
+      .connect(process.env.DB_URL as string, { dbName: process.env.DB_NAME })
+      .then((conn) => {
+        console.log(`Database Connected: ${conn.connection.host}`);
+      })
+      .catch((err) => {
+        console.error(`Database Connection Error: ${err.message}`);
+      });
+  },
+  close: () => {
+    mongoose.connection.close().then(() => {
+      console.log("Database Disconnected");
     });
+  },
 };
+
 export default dbConnection;
