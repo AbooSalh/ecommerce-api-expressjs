84acfcfdc653a1f5e915b213b1cdb0fc778b009a
AbooSalh <a.m0507979@gmail.com>
2025-03-09 15:33:39 +0200

    refactor: update category routes for consistency and clarity in endpoint structure

--- src/modules/category/routes.ts
+++ src/modules/category/routes.ts
@@ -2,13 +2,12 @@ import express from "express";
 import { categoryController as controller } from "./controller";
 import { categoryValidator as validator } from "./validators";
 const categoryRouter = express.Router();
+categoryRouter.route("/categories").get(controller.getAll);
 categoryRouter
-  .route("/categories")
-  .get(controller.getAll)
+  .route("/category")
   .post(validator.createCategory, controller.create);
-
 categoryRouter
-  .route("/categories/:title")
+  .route("/category/:title")
   .get(validator.getCategoryByTitle, controller.getOne)
   .put(validator.updateCategory, controller.update)
   .delete(validator.deleteCategory, controller.delete);
