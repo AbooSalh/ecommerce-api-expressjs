6fe2c2f9ed442c417740b1f4a9670dcd53240364
AbooSalh <a.m0507979@gmail.com>
2025-03-13 20:27:51 +0200

    feat: add Brand model with slug generation; implement pre-save and pre-update hooks for slug creation

---
+++ src/modules/Brands/model.ts
@@ -0,0 +1,58 @@
+import mongoose, { Schema } from "mongoose";
+import slugify from "slugify";
+export interface ICategory {
+  title: string;
+  image: string;
+}
+const brandSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "brand title is required"],
+      unique: [true, "brand title must be unique"],
+      minLength: [3, "brand title must be at least 3 characters"],
+      maxLength: [32, "brand title must be at most 32 characters"],
+    },
+    // api endpoint
+    slug: {
+      type: String,
+      lowercase: true,
+      unique: [true, "slug must be unique"],
+    },
+    image: {
+      type: String,
+    },
+  },
+  //   mongoose options
+  {
+    // timestamps for createdAt and updatedAt
+    timestamps: true,
+  }
+);
+
+// Pre-save hook to generate slug from title
+brandSchema.pre("save", function (next) {
+  if (this.isModified("title")) {
+    this.slug = slugify(this.title, { lower: true, strict: true });
+  }
+  next();
+});
+brandSchema.pre("findOneAndUpdate", async function (next) {
+  const update = this.getUpdate() as {
+    $set?: {
+      slug: string;
+      title?: string;
+    };
+  };
+
+  if (update.$set?.title) {
+    update.$set.slug = slugify(update.$set.title, {
+      lower: true,
+      strict: true,
+    });
+  }
+  next();
+});
+const BrandM = mongoose.model("Category", brandSchema);
+
+export default BrandM;
