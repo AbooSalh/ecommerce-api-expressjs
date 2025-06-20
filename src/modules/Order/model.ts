19023292a8ee417ebf6ca8c09683403f66714877
AbooSalh <a.m0507979@gmail.com>
2025-06-20 14:19:43 +0300

    feat: add Order model with schema definition for order management

---
+++ src/modules/Order/model.ts
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+type IOrder = {
+  user: mongoose.Schema.Types.ObjectId;
+  cart: mongoose.Schema.Types.ObjectId;
+  taxPrice: number;
+  shippingPrice: number;
+  totalOrderPrice: number;
+  paymentMethod: string;
+  isPaid: boolean;
+  paidAt: Date;
+  isDelivered: boolean;
+  deliveredAt: Date;
+};
+const OrderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    cart: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Cart",
+      required: true,
+    },
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    totalOrderPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+      enum: ["cod", "card"],
+      default: "cod",
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const OrderM = mongoose.model<IOrder>("Order", OrderSchema);
+
+export default OrderM;
